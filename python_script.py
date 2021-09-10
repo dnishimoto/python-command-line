a6858f4e388935bacd9cf3e8151de6a47b22834f
davidnishimoto@esiconstruction.com <davidnishimoto@esiconstruction.com>
2021-09-10 10:24:11 -0600

    upload

---
+++ python_script.py
@@ -0,0 +1,5 @@
+import sys
+import pandas as pd
+
+print(sys.version)
+print("hello world")
---
+++ test_row_to_list.py
@@ -0,0 +1,12 @@
+import pytest 
+
+def test_for_clean_row():
+    print("test for clean row")
+    assert False
+
+def test_for_clean_area():
+    print("test for clean area")
+    var=None
+    assert var is None
+
+
